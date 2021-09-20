484f1b05115bd927beb7498ebe4de483448ba149
Dinuka Hettiarachchi <dinukah@potenza.sg>
2021-09-20 13:24:06 +0530

    Login path created

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-import {LoginComponent} from './components/login/login.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
-  }, {
-    path: '',
-    component: LoginComponent,
+    component: AdminLayoutComponent,
     children: [{
       path: '',
       loadChildren: () => import('./layouts/admin-layout/admin-layout.module').then(m => m.AdminLayoutModule)
     }]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
@@ -26,7 +26,7 @@ const routes: Routes = [
     CommonModule,
     BrowserModule,
     RouterModule.forRoot(routes, {
-       useHash: true
+      useHash: true
     })
   ],
   exports: [
