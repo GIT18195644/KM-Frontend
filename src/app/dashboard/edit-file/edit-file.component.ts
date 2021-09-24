17ce335fe3e89af56b1b1a7b840575d4001ce51c
chamath7 <chamalakshan164@gmail.com>
2021-09-24 15:46:00 +0530

    edit file html

--- src/app/dashboard/edit-file/edit-file.component.ts
+++ src/app/dashboard/edit-file/edit-file.component.ts
@@ -1,5 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {FormGroup} from '@angular/forms';
 
 
 @Component({
@@ -12,6 +13,7 @@ export class EditFileComponent implements OnInit {
     rowCount: any;
     curPage: any;
     public rows = [];
+    editFileForm: FormGroup;
 
     constructor(private dialogRef: MatDialogRef<EditFileComponent>,
                 @Inject(MAT_DIALOG_DATA) public data ) {
@@ -20,4 +22,9 @@ export class EditFileComponent implements OnInit {
     ngOnInit(): void {
     }
 
+    onUpdateFile() {
+    }
+
+    onEditFile() {
+    }
 }
